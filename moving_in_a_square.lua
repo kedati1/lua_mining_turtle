53cc5e34671702416ed0025640ca650d53300baf
Kevin De Gouveia <degouveiakevin@gmail.com>
2024-08-16 11:39:36 +0100

    Minecraft turtle moves in a square

---
+++ moving_in_a_square.lua
@@ -0,0 +1,15 @@
+-- Make Turtle run in a square
+function moveSquare()
+    local start = 1
+    local length = 4
+    local total_sides = 4
+    local current_side = 0
+
+    while current_side < total_sides do
+        for x = start, length do
+            turtle.forward()
+        end    
+        turtle.turnRight()
+        current_side = current_side + 1;
+    end
+end
